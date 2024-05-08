1403a11e09a4862132f06e34434c745522c1b9ef
Dan01010011 <136166261+Dan01010011@users.noreply.github.com>
2024-05-07 22:13:01 -0300

    Update matrizMult.c

--- matrizMult.c
+++ matrizMult.c
@@ -14,47 +14,47 @@ int resultado[3][3];
 
 void *threadCode(void *tid){   
 	
-	int i,j, k;
-  int  threadId = (*(int *)tid); 
+	int i, j, k;
+  	int threadId = (*(int *)tid); 
 	
-	   for(i=threadId; i < LINHAS; i = i + NUM_THREADS) {
+	for(i=threadId; i < LINHAS; i = i + NUM_THREADS) {
 	   	      	
-      			for (j=0;j<COLUNAS;j++) {
-		        	 resultado[i][j] = 0;
+      	for (j=0;j<COLUNAS;j++) {
+		    resultado[i][j] = 0;
   
-       				 for(k=0;k< COLUNAS;k++) {
-           		 		resultado[i][j] = resultado[i][j] + matriz1[i][k]* matriz2[k][j];
-       		 		}
-      			}			
+       		for(k=0;k< COLUNAS;k++) {
+           		resultado[i][j] = resultado[i][j] + matriz1[i][k] * matriz2[k][j];
+       		}
+      	}			
        
-    	}
+    }
   
 }
 
 int main (int argc, char *argv[]){   
 	pthread_t threads[NUM_THREADS]; 
-  int *taskids[NUM_THREADS];
-	int i,j,u; int t;   
+  	int *taskids[NUM_THREADS];
+	int i, j, u, t;   
 	
-  for(t=0; t<NUM_THREADS; t++){
-    printf("No main: criando thread %d\n", t);    
-    taskids[t] = (int *) malloc(sizeof(int)); *taskids[t] = t;
+  	for(t=0; t<NUM_THREADS; t++){
+    	printf("No main: criando thread %d\n", t);    
+    	taskids[t] = (int *) malloc(sizeof(int)); *taskids[t] = t;
 	 	pthread_create(&threads[t],NULL,threadCode, (void *)taskids[t]);         
-  }
+  	}
   
-  for(u=0; u<NUM_THREADS;u++) {
-    int *res;
-    pthread_join(threads[u], NULL);
-  }   
+  	for(u=0; u<NUM_THREADS;u++) {
+    	int *res;
+    	pthread_join(threads[u], NULL);
+  	}   
   
-  for(i=0; i < LINHAS; i++) {
-			 for (j=0;j<COLUNAS;j++) {
-      		 printf("%d\t", resultado[i][j]);
-  		 }
+  	for(i=0; i < LINHAS; i++) {
+		for(j=0;j<COLUNAS;j++) {
+      		printf("%d\t", resultado[i][j]);
+  		}
   		 
-  		 printf("\n");
-   }
+  		printf("\n");
+   	}
         		
   
-  pthread_exit(NULL);
+  	pthread_exit(NULL);
 }
